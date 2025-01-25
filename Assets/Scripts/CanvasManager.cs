0eaeb9c588d6d45ca970d65763105203a991521c
arif-vikka <151728261+arif-vikka@users.noreply.github.com>
2025-01-26 02:02:52 +0300

    canvvas updating

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -7,20 +7,57 @@ using UnityEngine.UI;
 public class CanvasManager : MonoBehaviour
 {
     public Image vibeFillImage;
+    public Slider vibeSlider;
     public GameObject starvationPanel;
     public GameObject caughtPanel;
 
-    public TMP_Text scoreText;
+    public TMP_Text timerText;
 
+    public bool isTimerActive;
+
+    public float timeStart;
+    public float currentTimePassed;
+    
     private void Start()
     {
         starvationPanel.SetActive(false);
         caughtPanel.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (isTimerActive)
+        {
+            Timer();
+        }
+    }
+
+    public void StartTimer()
+    {
+        isTimerActive = true;
+        timeStart = Time.time;
+    }
+
+
+    public void Timer()
+    {
+        //currentTimePassed = Time.time - timerStart;
+    }
+    
+    public void StopTimer()
+    {
+        isTimerActive = false;
+    }
+
+    public void TimerText()
+    {
+        
+    }
+
     public void SetVibeFill()
     {
-        vibeFillImage.fillAmount = GameManager.Instance.vibe.currentValue / GameManager.Instance.vibe.maxValue;
+        //vibeFillImage.fillAmount = GameManager.Instance.vibe.currentValue / GameManager.Instance.vibe.maxValue;
+        vibeSlider.value = GameManager.Instance.vibe.currentValue / GameManager.Instance.vibe.maxValue;
     }
 
     public void SetStarvationPanelActive()
@@ -35,6 +72,6 @@ public class CanvasManager : MonoBehaviour
 
     public void IncreaseScore(int amount)
     {
-        scoreText.text = amount.ToString();
+        timerText.text = amount.ToString();
     }
 }
