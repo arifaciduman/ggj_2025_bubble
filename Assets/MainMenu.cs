75d6d561aa3fd94fd5bb42a75df861ff360ce1a7
mugetsuxk <bilgekaganaktas@hotmail.com>
2025-01-26 13:57:46 +0300

    cinematic

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject howToPanel;
     public GameObject creditsPanel;
     public GameObject mainPanel;
-    
+
+    public VideoPlayer videoPlayer;
+
+    public GameObject cinematicParent;
+    public GameObject blackImage;
+
+    public CanvasGroup canvasGroup;
+
     public void PlayButton()
     {
-        SceneManager.LoadScene(1);
+        blackImage.SetActive(true);
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        void DelayCinematic()
+        {
+            cinematicParent.SetActive(true);
+            videoPlayer.playbackSpeed = 1;
+            videoPlayer.Play();
+            System.Action action = () => SceneManager.LoadScene(1);
+            DelayUtility.ExecuteAfterSeconds(action, (float)videoPlayer.clip.length, true);
+        }
+        DelayUtility.ExecuteAfterFrames(DelayCinematic, 3, true);
     }
 
     public void HowTo()
